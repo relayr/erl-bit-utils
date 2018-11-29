63341ea4f41502646a2a6020431ac30142c25a73
Kuba Odias <kuba.odias@relayr.io>
2018-11-29 06:41:19 +0100

    Bit operating functions with tests

---
+++ src/bit_utils.erl
@@ -0,0 +1,88 @@
+%%------------------------------------------------------------------------------
+%% @author kuba.odias
+%% @copyright relayr 2009-2018
+%% @doc Miscellaneous functions for Erlang bits processing.
+%% @end
+%%------------------------------------------------------------------------------
+-module(bit_utils).
+
+%%------------------------------------------------------------------------------
+%% Include files
+%%------------------------------------------------------------------------------
+
+%%------------------------------------------------------------------------------
+%% Function exports
+%%------------------------------------------------------------------------------
+-export([
+    bits_set/2,
+    set_bits/2,
+    lowest_bit_set/1,
+    bytes_to_bits/1,
+    bits_to_bytes/1,
+    rest_of_byte_size_alignment/1
+]).
+
+%% =============================================================================
+%% Exported functions
+%% =============================================================================
+
+-spec bits_set(Integer :: non_neg_integer(), Length :: pos_integer()) ->
+    {ok, [non_neg_integer(), ...]}.
+bits_set(Integer, Length) ->
+    bits_set(Integer, Length, []).
+
+-spec bits_set(Integer :: non_neg_integer(), Length :: non_neg_integer(), BitList :: [non_neg_integer()]) ->
+    {ok, [non_neg_integer(), ...]}.
+bits_set(_Integer, 0, BitList) ->
+    {ok, BitList};
+bits_set(Integer, Length, BitList) ->
+    NewBitList =
+        if Integer rem 2 =:= 0 ->
+            BitList;
+        Integer rem 2 =:= 1 ->
+            [Length - 1 | BitList]
+        end,
+    bits_set(Integer bsr 1, Length - 1, NewBitList).
+
+-spec set_bits(BitList :: [non_neg_integer()], Length :: pos_integer()) -> {ok, non_neg_integer()}.
+set_bits(BitList, Length) ->
+    set_bits(BitList, Length, 0).
+
+-spec lowest_bit_set(N :: non_neg_integer()) -> LowestBitSet :: non_neg_integer().
+lowest_bit_set(0) ->
+    0;
+lowest_bit_set(N) ->
+    lowest_bit_set(N, 1).
+
+-spec bytes_to_bits(Bytes :: integer()) -> Bits :: integer().
+bytes_to_bits(Bytes) ->
+    Bytes*8.
+
+-spec bits_to_bytes(Bits :: integer()) -> Bytes :: integer().
+bits_to_bytes(Bits) ->
+    trunc(math:ceil(Bits/8)).
+
+%% @doc Get rest number of bits when aligned to a byte.
+rest_of_byte_size_alignment(BitSize) when (BitSize rem 8 =:= 0) ->
+    0;
+rest_of_byte_size_alignment(BitSize) ->
+    (BitSize div 8 + 1) * 8 - BitSize.
+
+%% =============================================================================
+%% Local functions
+%% =============================================================================
+
+-spec set_bits(BitList :: [non_neg_integer()], Length :: non_neg_integer(), Integer :: non_neg_integer()) ->
+    {ok, non_neg_integer()}.
+set_bits([], _Length, Integer) ->
+    {ok, Integer};
+set_bits([Bit | RestOfBitList], Length, Integer) ->
+    set_bits(RestOfBitList, Length, Integer + (1 bsl (Length - Bit - 1))).
+
+-spec lowest_bit_set(N :: non_neg_integer(), Pos :: non_neg_integer()) -> LowestBitSet :: non_neg_integer().
+lowest_bit_set(N, Pos) ->
+    if N rem 2 =:= 1 ->
+        Pos;
+    true ->
+        lowest_bit_set(N bsr 1, Pos + 1)
+    end.
---
+++ test/bit_utils_tests.erl
@@ -0,0 +1,72 @@
+%%------------------------------------------------------------------------------
+%% @author kuba.odias
+%% @copyright relayr 2009-2018
+%%------------------------------------------------------------------------------
+-module(bit_utils_tests).
+
+-ifdef(TEST).
+
+%%------------------------------------------------------------------------------
+%% Include files
+%%------------------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+
+%% =============================================================================
+%% Unit tests
+%% =============================================================================
+
+bits_to_bytes_test() ->
+    Res1 = bit_utils:bits_to_bytes(2),
+    ?assertEqual(1,Res1),
+    Res2 = bit_utils:bits_to_bytes(128),
+    ?assertEqual(16,Res2).
+
+bytes_to_bits_test() ->
+    Res1 = bit_utils:bytes_to_bits(1),
+    ?assertEqual(8,Res1),
+    Res2 = bit_utils:bytes_to_bits(128),
+    ?assertEqual(1024,Res2).
+
+lowest_bit_set_test() ->
+    ?assertEqual(0, bit_utils:lowest_bit_set(0)),
+    ?assertEqual(1, bit_utils:lowest_bit_set(1)),
+    ?assertEqual(1, bit_utils:lowest_bit_set(3)),
+    ?assertEqual(4, bit_utils:lowest_bit_set(8)),
+    ?assertEqual(1, bit_utils:lowest_bit_set(9)).
+
+bits_set_test() ->
+    Len1 = 0,
+    <<Bits1:Len1>> = <<>>,
+    {ok, R1} = bit_utils:bits_set(Bits1, Len1),
+    ?assertEqual([], R1),
+
+    Len2 = 3,
+    {ok, R2} = bit_utils:bits_set(2#000, Len2),
+    ?assertEqual([], R2),
+
+    Len3 = 5,
+    {ok, R3} = bit_utils:bits_set(2#10010, Len3),
+    ?assertEqual([0, 3], R3),
+
+    Len4 = 3,
+    {ok, R4} = bit_utils:bits_set(2#110, Len4),
+    ?assertEqual([0, 1], R4).
+
+set_bits_test() ->
+    Len1 = 0,
+    {ok, R1} = bit_utils:set_bits([], Len1),
+    ?assertEqual(0, R1),
+
+    Len2 = 4,
+    {ok, R2} = bit_utils:set_bits([], Len2),
+    ?assertEqual(2#0000, R2),
+
+    Len3 = 6,
+    {ok, R3} = bit_utils:set_bits([2,5], Len3),
+    ?assertEqual(2#001001, R3),
+
+    Len4 = 3,
+    {ok, R4} = bit_utils:set_bits([0,1], Len4),
+    ?assertEqual(2#110, R4).
+
+-endif.
